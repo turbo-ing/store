fdf7cb7d41682f09bb690f04bcf17fb2b0686a15
Neo-Gar <nvatlecov@mail.ru>
2024-09-19 11:48:23 +0800

    Removed import

--- packages/sdk/components/widgets/Header/Header.tsx
+++ packages/sdk/components/widgets/Header/Header.tsx
@@ -1,6 +1,5 @@
 import MobileNavbar from '../../../components/widgets/Header/MobileNavbar';
 import DesktopNavbar from '../../../components/widgets/Header/DesktopNavbar';
-import {useAccountStore} from "../../../lib/stores/accountStore";
 
 export default function Header() {
   return (
