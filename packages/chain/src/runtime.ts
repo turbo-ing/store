9a19ce706066bbdccf455eb78a9f92eaffcd5701
asimaranov <a.simaranov@gmail.com>
2024-09-26 01:44:06 +0700

    Decoupling: separated games contracts

--- packages/chain/src/runtime.ts
+++ packages/chain/src/runtime.ts
@@ -1,33 +1,6 @@
-import { UInt64 } from '@proto-kit/library';
-import { ArkanoidGameHub } from './games/arkanoid/ArkanoidGameHub';
-import { RandzuLogic } from './games/randzu/RandzuLogic';
-import { ThimblerigLogic } from './games/thimblerig/ThimblerigLogic';
-import { Balances } from '@zknoid/chain-sdk/dist/framework';
-import { ModulesConfig } from '@proto-kit/common';
-import { CheckersLogic } from './games/checkers';
-import { GuessGame } from './games/number_guessing';
-
-const modules = {
-  ArkanoidGameHub,
-  ThimblerigLogic,
-  Balances,
-  RandzuLogic,
-  CheckersLogic,
-  GuessGame,
-};
-
-const config: ModulesConfig<typeof modules> = {
-  ArkanoidGameHub: {},
-  ThimblerigLogic: {},
-  Balances: {
-    totalSupply: UInt64.from(10000),
-  },
-  RandzuLogic: {},
-  CheckersLogic: {},
-  GuessGame: {},
-};
+import D from '@zknoid/chain-games/dist/src/runtime';
 
 export default {
-  modules,
-  config,
+  modules: D.modules,
+  config: D.config,
 };
