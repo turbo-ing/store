22f6a1225e1ce5c64ac5bc483363c397c4c90bbc
asimaranov <a.simaranov@gmail.com>
2024-09-27 13:39:59 +0700

    Fixed dev run of the project

--- packages/chain/scripts/get-analyze-info.ts
+++
@@ -1,3 +0,0 @@
-import { GameRecord } from '../src/games/arkanoid/ArkanoidGameHub';
-
-console.log(GameRecord.analyzeMethods());
--- packages/chain/scripts/get-verification-key.ts
+++
@@ -1,3 +0,0 @@
-import { GameRecord } from '../src/games/arkanoid/ArkanoidGameHub';
-
-console.log(await GameRecord.compile());
