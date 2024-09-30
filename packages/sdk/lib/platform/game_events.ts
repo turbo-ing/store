2317928c25e5d0af11af3297a62a05dd68a898bc
Neo-Gar <nvatlecov@mail.ru>
2024-09-30 19:59:36 +0700

    Added events & slide

--- packages/sdk/lib/platform/game_events.ts
+++ packages/sdk/lib/platform/game_events.ts
@@ -1,11 +1,11 @@
-import { DateTime, DurationObjectUnits, Interval } from 'luxon';
-import { useEffect, useState } from 'react';
+import { DateTime, DurationObjectUnits, Interval } from "luxon";
+import { useEffect, useState } from "react";
 
 export enum ZkNoidEventType {
-  PAST_EVENTS = 'Past events',
-  CURRENT_EVENTS = 'Current events',
-  UPCOMING_EVENTS = 'Upcoming events',
-  PREREGISTRAION = 'Preregistration of the event',
+  PAST_EVENTS = "Past events",
+  CURRENT_EVENTS = "Current events",
+  UPCOMING_EVENTS = "Upcoming events",
+  PREREGISTRAION = "Preregistration of the event",
 }
 
 export const ALL_GAME_EVENT_TYPES = [
@@ -22,25 +22,60 @@ export type ZkNoidEvent = {
   eventEnds: number;
   link: string;
   image?: string;
+  imageFullWidth?: boolean;
+  textColor?: "white" | "black";
 };
 
 export const GAME_EVENTS: ZkNoidEvent[] = [
   {
-    name: 'TileVille Challenge',
+    name: "TileVille Challenge",
     description:
-      'Three-day city-builder challenge in strategic city-builder in game TileVille',
-    eventStarts: new Date('2024-06-21').getTime(),
-    eventEnds: new Date('2024-06-24').getTime(),
-    link: 'https://www.tileville.xyz/',
-    image: '/image/events/banner/tileville_banner.svg',
+      "Three-day city-builder challenge in strategic city-builder in game TileVille",
+    eventStarts: new Date("2024-06-21").getTime(),
+    eventEnds: new Date("2024-06-24").getTime(),
+    link: "https://www.tileville.xyz/",
+    image: "/image/events/banner/tileville_banner.svg",
   },
   {
-    name: 'Checkers game test',
-    description: 'Play chess, find bugs, report bugs and grab reward!',
-    eventStarts: new Date('2024-06-30').getTime(),
-    eventEnds: new Date('2024-11-30').getTime(),
-    link: '/games/arkanoid/0',
-    image: '/image/events/banner/checkers_banner.svg',
+    name: "Checkers game test",
+    description: "Play chess, find bugs, report bugs and grab reward!",
+    eventStarts: new Date("2024-06-30").getTime(),
+    eventEnds: new Date("2024-09-10").getTime(),
+    link: "/games/arkanoid/0",
+    image: "/image/events/banner/checkers_banner.svg",
+  },
+  {
+    name: "ETHGlobal Online Hack",
+    description:
+      "Discover the world of provable, zero-knowledge game development on Mina Protocol! Create your own fair game environment based on the modular “plug and play” SDK From ZkNoid.",
+    eventStarts: new Date("2024-08-23").getTime(),
+    eventEnds: new Date("2024-09-13").getTime(),
+    link: "https://ethglobal.com/events/ethonline2024",
+    image: "/image/events/eth-online.svg",
+    imageFullWidth: true,
+    textColor: "black",
+  },
+  {
+    name: "ETHGlobal Singapore Hack",
+    description:
+      "Discover the world of provable, zero-knowledge game development on Mina Protocol! Create your own fair game environment based on the modular “plug and play” SDK From ZkNoid.",
+    eventStarts: new Date("2024-09-20").getTime(),
+    eventEnds: new Date("2024-09-22").getTime(),
+    link: "https://ethglobal.com/events/singapore2024",
+    image: "/image/events/eth-singapore.svg",
+    imageFullWidth: true,
+    textColor: "black",
+  },
+  {
+    name: "ZkNoid Bounty for Gamers",
+    description:
+      "To participate in the bounty, simply record your ZkNoid gaming journey with any screen recording software and send it to us. Be creative, using editing software is encouraged! Don’t forget to describe your experience in a comment to the submission.",
+    eventStarts: new Date("2024-09-13").getTime(),
+    eventEnds: new Date("2024-10-04").getTime(),
+    link: "https://github.com/ZkNoid/zknoid/issues/13",
+    image: "/image/events/astromina.svg",
+    imageFullWidth: true,
+    textColor: "white",
   },
 ];
 
@@ -67,7 +102,7 @@ const getEventTime = (event: ZkNoidEvent) => {
   }
 
   return Interval.fromDateTimes(now, DateTime.fromMillis(comparedTime))
-    .toDuration(['days', 'hours', 'minutes', 'seconds'])
+    .toDuration(["days", "hours", "minutes", "seconds"])
     .toObject();
 };
 
