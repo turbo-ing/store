1bf84ef3e10f0f86ce85c462c0c948f120f5249a
asimaranov <a.simaranov@gmail.com>
2024-10-14 16:33:07 +0700

    Fixed factory address

--- packages/games/lottery/constants/addresses.ts
+++ packages/games/lottery/constants/addresses.ts
@@ -1,17 +1,9 @@
 import { NETWORKS, NetworkIds } from '@zknoid/sdk/constants/networks';
 
-export const LOTTERY_ADDRESS: {
-  readonly [networkId: string]: string | 'not-deployed';
-} = {
-  [NetworkIds.MINA_DEVNET]:
-    'B62qm75zXnsTi9eM6mohWWwfMXhiR2WMeRFB2ecFHjeYMKHs2c1bqtk',
-  [NetworkIds.ZEKO_TESTNET]: 'not-deployed',
-};
-
 export const FACTORY_ADDRESS: {
   readonly [networkId: string]: string | 'not-deployed';
 } = {
   [NetworkIds.MINA_DEVNET]:
-    'B62qpDzNdAAX8yvrcqFPDrwXbKy6mgZDusFgaq5hcgf35AaQDQAGRVF',
+    'B62qm75zXnsTi9eM6mohWWwfMXhiR2WMeRFB2ecFHjeYMKHs2c1bqtk',
   [NetworkIds.ZEKO_TESTNET]: 'not-deployed',
 };
