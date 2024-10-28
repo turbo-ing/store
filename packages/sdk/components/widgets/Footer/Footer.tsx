49274b9702864006d9b768d590caebf63e2f7fb7
Neo-Gar <nvatlecov@mail.ru>
2024-10-28 14:13:23 +0700

    Fixed Footer

--- packages/sdk/components/widgets/Footer/Footer.tsx
+++ packages/sdk/components/widgets/Footer/Footer.tsx
@@ -5,93 +5,78 @@ import { SOCIALS } from "../../../constants/socials";
 
 export default function Footer() {
   return (
-    <div
-      className={
-        "mx-[2.604vw] mb-[2.604vw] mt-[5.208vw] flex flex-col justify-between gap-[0.833vw]"
-      }
-    >
-      <div
-        className={"grid grid-cols-3 grid-rows-2 gap-[0.833vw] lg:grid-rows-1"}
-      >
-        <Link
-          href={"/"}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={
-            "col-start-1 col-end-4 w-fit cursor-pointer ease-in-out hover:opacity-80"
-          }
-        >
-          <Image
-            src={zknoidLogo}
-            alt={"ZkNoid Logo"}
-            className={"h-full w-[11.458vw]"}
-          />
-        </Link>
+    <div className="mx-[2.604vw] mb-[2.604vw] mt-[5.208vw] flex flex-col gap-[1.979vw]">
+      <div className={"grid grid-cols-3 w-full"}>
+        <div className={"w-full"}>
+          <Link
+            href={"/"}
+            target={"_blank"}
+            rel="noopener noreferrer"
+            className={"hover:opacity-80"}
+          >
+            <Image
+              src={zknoidLogo}
+              alt={"Zknoid logo"}
+              className={"h-full w-[11.458vw]"}
+            />
+          </Link>
+        </div>
         <div
-          className={
-            "col-start-1 col-end-4 grid grid-cols-2 grid-rows-2 flex-row items-center justify-center gap-x-[0.833vw] gap-y-[0.208vw] self-center lg:flex"
-          }
+          className={"flex flex-row items-center justify-center gap-[2.083vw]"}
         >
           <Link
-            className="cursor-pointer font-museo text-[0.833vw] font-medium ease-in-out hover:opacity-80 lg:even:text-right"
             href={"https://zknoid.io"}
-            target="_blank"
+            target={"_blank"}
             rel="noopener noreferrer"
+            className={"hover:opacity-80 font-museo text-[0.833vw] font-medium"}
           >
             About us
           </Link>
           <Link
-            className="cursor-pointer font-museo text-[0.833vw] font-medium ease-in-out hover:opacity-80 lg:even:text-right"
             href={"https://github.com/ZkNoid"}
-            target="_blank"
+            target={"_blank"}
             rel="noopener noreferrer"
+            className={"hover:opacity-80 font-museo text-[0.833vw] font-medium"}
           >
             Github
           </Link>
           <Link
-            className="cursor-pointer font-museo text-[0.833vw] font-medium ease-in-out hover:opacity-80 lg:even:text-right"
             href={"https://zknoid.medium.com"}
-            target="_blank"
+            target={"_blank"}
             rel="noopener noreferrer"
+            className={"hover:opacity-80 font-museo text-[0.833vw] font-medium"}
           >
             Blog
           </Link>
           <Link
-            className="cursor-pointer font-museo text-[0.833vw] font-medium ease-in-out hover:opacity-80 lg:even:text-right"
             href={"https://docs.zknoid.io"}
-            target="_blank"
+            target={"_blank"}
             rel="noopener noreferrer"
+            className={"hover:opacity-80 font-museo text-[0.833vw] font-medium"}
           >
             Documentation
           </Link>
         </div>
+        <div />
       </div>
-      <div
-        className={
-          "grid grid-cols-3 grid-rows-2 gap-y-[1.667vw] lg:grid-rows-1 lg:gap-y-0"
-        }
-      >
-        <div className="col-start-1 col-end-4 flex gap-[0.625vw]">
-          {SOCIALS.map((x) => (
+      <div className={"flex flex-row items-center justify-between w-full"}>
+        <div className={"flex flex-row items-center gap-[0.833vw]"}>
+          {SOCIALS.map((item) => (
             <Link
-              href={x.link}
-              key={x.id}
+              href={item.link}
+              key={item.id}
               className={"flex items-center justify-center hover:opacity-80"}
             >
               <Image
-                alt={x.name}
-                src={x.whiteImage}
+                alt={item.name}
+                src={item.whiteImage}
                 className={"h-[1.25vw] w-[1.25vw]"}
               />
             </Link>
           ))}
         </div>
-        <div
-          className={
-            "col-start-1 col-end-4 row-start-2 font-mono text-[0.625vw] font-light lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:text-end lg:text-[0.729vw]"
-          }
-        >
-          © 2024 ZkNoid: all rights reserved
+        <div className={"font-mono text-[0.625vw] font-light"}>
+          © {new Date().getFullYear()} ZkNoid: all rights reserved
         </div>
       </div>
     </div>
