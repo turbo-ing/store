8d75c74741d13eec3eb9e0bf7738af11d33f93e7
asimaranov <a.simaranov@gmail.com>
2024-12-03 10:12:16 +0700

    Fixed export for connect4 game

--- packages/chain-games/src/index.ts
+++ packages/chain-games/src/index.ts
@@ -3,3 +3,4 @@ export * from './checkers/index.js';
 export * from './arkanoid/index.js';
 export * from './thimblerig/index.js';
 export * from './number_guessing/index.js';
+export * from './connect_4/index.js';
--- packages/chain/src/index.ts
+++ packages/chain/src/index.ts
@@ -3,10 +3,7 @@ import * as ProtokitLibrary from '@proto-kit/library';
 import { UInt64 } from '@proto-kit/library';
 
 export * from '@zknoid/chain-games';
-
 export * from '@zknoid/chain-sdk';
-export * from '@zknoid/chain-sdk';
-
 export * from './environments/client.config';
 
 export { ClientAppChain, ProtokitLibrary, UInt64 as ProtoUInt64 };
