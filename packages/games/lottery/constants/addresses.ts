7e6f80caee601330b9cfa043aef3714bdebf530f
asimaranov <a.simaranov@gmail.com>
2024-12-16 21:22:16 +0800

    Updated lottery factory address

--- packages/games/lottery/constants/addresses.ts
+++ packages/games/lottery/constants/addresses.ts
@@ -4,7 +4,7 @@ export const FACTORY_ADDRESS: {
   readonly [networkId: string]: string | 'not-deployed';
 } = {
   [NetworkIds.MINA_DEVNET]:
-    'B62qoJ59ASv7orJxqM7uVXeyeBf2C3TaPC3rcCe1viJCrQnUb5Hv65X',
+    'B62qpxvBxZtFZohm9BVnqnVoWiaZP92yjXtrPwmKFf7QvPK12aYZJVy',
   [NetworkIds.MINA_MAINNET]: 'B62qoJ59ASv7orJxqM7uVXeyeBf2C3TaPC3rcCe1viJCrQnUb5Hv65X',
   [NetworkIds.ZEKO_TESTNET]: 'not-deployed',
 };
