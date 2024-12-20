b9c52c325c12e1c4356b39ab8d8180630d10001b
asimaranov <a.simaranov@gmail.com>
2024-12-20 18:54:49 +0800

    Updated contracts

--- packages/games/lottery/constants/addresses.ts
+++ packages/games/lottery/constants/addresses.ts
@@ -5,6 +5,6 @@ export const FACTORY_ADDRESS: {
 } = {
   [NetworkIds.MINA_DEVNET]:
     'B62qpxvBxZtFZohm9BVnqnVoWiaZP92yjXtrPwmKFf7QvPK12aYZJVy',
-  [NetworkIds.MINA_MAINNET]: 'B62qp1AZXxLWvYaKT5cNJzuLCRXfBBn9HwNLwebbcRLaiyNuuJNXkLk',
+  [NetworkIds.MINA_MAINNET]: 'B62qkmwNKgW5TsU95HhzLd1jFAEpsiGG27aiga1crLaUkPBeR4pZ5FU',
   [NetworkIds.ZEKO_TESTNET]: 'not-deployed',
 };
