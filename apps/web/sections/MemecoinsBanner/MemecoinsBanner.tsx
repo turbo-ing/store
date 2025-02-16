fcc2154cdbea5f26a7166e96610968dafd76ef88
Neo-Gar <nvatlecov@mail.ru>
2025-02-16 21:41:24 +0200

    Added BuyModal

--- apps/web/sections/MemecoinsBanner/MemecoinsBanner.tsx
+++ apps/web/sections/MemecoinsBanner/MemecoinsBanner.tsx
@@ -5,12 +5,25 @@ import memeBannerIMG from "@/public/image/memecoins/banner.svg";
 import coinFrogIMG from "@/public/image/memecoins/coin-frog.svg";
 import coinDragonIMG from "@/public/image/memecoins/сoin-dragon.svg";
 import minanftIMG from "@/public/image/partners/minanft.svg";
+import minaICON from "@/public/image/memecoins/mina.svg";
+import dragonICON from "@/public/image/memecoins/dragon.svg";
+import frogICON from "@/public/image/memecoins/frog.svg";
 import { DateTime, DurationObjectUnits, Interval } from "luxon";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { cn, formatAddress } from "@zknoid/sdk/lib/helpers";
 import { useNetworkStore } from "@zknoid/sdk/lib/stores/network";
 import Link from "next/link";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTriggerChevron,
+  SelectValue,
+} from "../../../../packages/sdk/components/shared/Select/Select";
+import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
 function TimerItem({ time, text }: { time: number; text: string }) {
   return (
@@ -219,6 +232,7 @@ function CoinBock({
   image,
   link,
   btnColor,
+  onBuy,
 }: {
   label: string;
   price: number;
@@ -227,6 +241,7 @@ function CoinBock({
   image: any;
   link: string;
   btnColor: string;
+  onBuy: () => void;
 }) {
   const networkStore = useNetworkStore();
   const sortedLeaderboard = leaderboard.sort((a, b) => b.amount - a.amount);
@@ -447,6 +462,7 @@ function CoinBock({
               "hover:opacity-80 rounded-[0.26vw] flex flex-col justify-center items-center w-full py-[0.26vw] mt-[0.26vw]"
             }
             style={{ backgroundColor: btnColor }}
+            onClick={onBuy}
           >
             <span
               className={"text-[1.25vw] font-museo font-medium text-foreground"}
@@ -621,6 +637,185 @@ function RulesModal({ onClose }: { onClose: () => void }) {
   );
 }
 
+function BuyModal({
+  defaultValue,
+  onClose,
+}: {
+  defaultValue: "frog" | "dragon";
+  onClose: () => void;
+}) {
+  const frogPrice = 1;
+  const dragonPrice = 1;
+
+  const [choosenCoin, setChoosenCoin] = useState<"frog" | "dragon">(
+    defaultValue,
+  );
+  const [buyAmount, setBuyAmount] = useState<number>(1);
+
+  const validationSchema = Yup.object().shape({
+    amount: Yup.number().min(1).required("Amount is required"),
+  });
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ type: "spring", duration: 0.4, bounce: 0 }}
+      className={
+        "fixed left-0 top-0 z-50 flex h-full w-full flex-col items-center justify-center backdrop-blur-md p-[10vw] lg:!p-0"
+      }
+      onClick={onClose}
+    >
+      <div
+        className={
+          "min-h-[20.833vw] min-w-[28.646vw] relative flex flex-col rounded-[0.781vw] bg-bg-dark p-[1.563vw]"
+        }
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div
+          className={
+            "absolute cursor-pointer top-[0.26vw] hover:opacity-80 -right-[1.563vw] flex flex-col justify-center items-center"
+          }
+          onClick={onClose}
+        >
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={"w-[1.042vw] h-[1.042vw]"}
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M11.8174 10.0043L20 1.81818L18.1826 0L9.99994 8.18609L1.81742 0.000150223L0 1.81833L8.18252 10.0043L0.00836661 18.1818L1.82579 20L9.99994 11.8225L18.1742 20.0002L19.9917 18.182L11.8174 10.0043Z"
+              fill="#141414"
+            />
+          </svg>
+        </div>
+        <Formik
+          initialValues={{ amount: buyAmount }}
+          validationSchema={validationSchema}
+          onSubmit={() => {}}
+        >
+          {({ errors, touched }) => (
+            <Form className={"flex flex-col"}>
+              <div
+                className={
+                  "w-full text-center mb-[0.521vw] text-[1.667vw] font-museo font-bold text-foreground"
+                }
+              >
+                Mint {choosenCoin == "frog" ? "Frozen Frog" : "Fire Dragon"}{" "}
+                Token
+              </div>
+              <div className={"flex flex-col gap-[0.26vw] mb-[1.042vw]"}>
+                <span
+                  className={
+                    "text-[0.833vw] font-plexsans text-foreground leading-[110%]"
+                  }
+                >
+                  From
+                </span>
+                <div
+                  className={
+                    "rounded-[0.521vw] bg-[#252525] p-[0.781vw] flex flex-row items-center"
+                  }
+                >
+                  <Image
+                    src={minaICON}
+                    alt={"Mina icon"}
+                    className={"w-[1.771vw] h-[1.771vw]"}
+                  />
+                  <span
+                    className={
+                      "ml-[0.521vw] text-foreground text-[1.042vw] font-medium font-plexsans leading-[110%] uppercase"
+                    }
+                  >
+                    MINA
+                  </span>
+                  <span
+                    className={
+                      "ml-auto mr-[0.521vw] text-foreground text-[1.042vw] font-medium font-plexsans leading-[110%] uppercase"
+                    }
+                  >
+                    {choosenCoin === "frog"
+                      ? buyAmount * frogPrice
+                      : buyAmount * dragonPrice}
+                  </span>
+                </div>
+                <div className={"w-full h-px my-[1.042vw] bg-[#252525]"} />
+                <span
+                  className={
+                    "text-[0.833vw] font-plexsans text-foreground leading-[110%]"
+                  }
+                >
+                  To
+                </span>
+                <div
+                  className={
+                    "rounded-[0.521vw] bg-[#252525] p-[0.781vw] flex flex-row items-center"
+                  }
+                >
+                  <Image
+                    src={choosenCoin === "frog" ? frogICON : dragonICON}
+                    alt={"Coin icon"}
+                    className={"w-[1.771vw] h-[1.771vw]"}
+                  />
+                  <Select
+                    value={choosenCoin}
+                    onValueChange={(value: "frog" | "dragon") => {
+                      setChoosenCoin(value);
+                      setBuyAmount(1);
+                    }}
+                  >
+                    <SelectTriggerChevron className="ml-[0.521vw] text-foreground text-[1.042vw] font-medium font-plexsans leading-[110%] uppercase">
+                      <SelectValue />
+                    </SelectTriggerChevron>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectItem value={"frog"}>Frog</SelectItem>
+                        <SelectItem value={"dragon"}>Dragon</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <Field
+                    name={"amount"}
+                    value={buyAmount}
+                    type={"number"}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                      setBuyAmount(Number(e.target.value));
+                    }}
+                    className={
+                      "text-right ml-auto outline-none appearance-none focus:outline-none bg-[#252525] mr-[0.521vw] text-foreground text-[1.042vw] font-medium font-plexsans leading-[110%] uppercase"
+                    }
+                  />
+                </div>
+              </div>
+              <button
+                type={"submit"}
+                className={cn(
+                  "rounded-[0.26vw] py-[0.521vw] flex flex-col justify-center items-center w-full",
+                  choosenCoin == "frog" ? "bg-[#3A39FF]" : "bg-[#FF5B23]",
+                )}
+              >
+                <span
+                  className={
+                    "text-[1.25vw] font-museo font-medium text-foreground"
+                  }
+                >
+                  Mint {choosenCoin == "frog" ? "Frozen Frog" : "Fire Dragon"}
+                </span>
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </motion.div>
+  );
+}
+
 export default function MemecoinsBanner() {
   const event = {
     date: {
@@ -632,6 +827,10 @@ export default function MemecoinsBanner() {
   const frozenCoinAmount = 1200;
   const hotCoinAmount = 994;
   const [isRulesOpen, setIsRulesOpen] = useState<boolean>(false);
+  const [isBuyModalOpen, setIsBuyModalOpen] = useState<boolean>(false);
+  const [touchedCoin, setTouchedCoin] = useState<"frog" | "dragon" | undefined>(
+    undefined,
+  );
 
   const getTimeLeft = () => {
     return Interval.fromDateTimes(
@@ -752,6 +951,10 @@ export default function MemecoinsBanner() {
           image={coinFrogIMG}
           link={"#"}
           btnColor={"#3A39FF"}
+          onBuy={() => {
+            setTouchedCoin("frog");
+            setIsBuyModalOpen(true);
+          }}
         />
         <CoinBock
           label={"Fire Dragon"}
@@ -761,12 +964,25 @@ export default function MemecoinsBanner() {
           image={coinDragonIMG}
           link={"#"}
           btnColor={"#FF5B23"}
+          onBuy={() => {
+            setTouchedCoin("dragon");
+            setIsBuyModalOpen(true);
+          }}
         />
       </div>
       {isRulesOpen && (
         <RulesModal
           onClose={() => {
             setIsRulesOpen(false);
+          }}
+        />
+      )}
+      {isBuyModalOpen && touchedCoin && (
+        <BuyModal
+          defaultValue={touchedCoin}
+          onClose={() => {
+            setIsBuyModalOpen(false);
+            setTouchedCoin(undefined);
           }}
         />
       )}
